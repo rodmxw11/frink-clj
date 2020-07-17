f994d47d2b948991cb9eb6232714ef10aa04b12c
Rodney Maxwell <rodmxw11@gmail.com>
2020-07-17 19:37:38 -0400

    Able to call frink.jar

--- src/frink_clj/core.clj
+++ src/frink_clj/core.clj
@@ -1,7 +1,15 @@
 (ns frink-clj.core
+  (:import frink.parser.Frink)
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (let [
+         frink (new frink.parser.Frink)
+         results (.parseString frink "2 + 3/4 + 9/17")
+       ]
+       (println results)
+   )  
+)
